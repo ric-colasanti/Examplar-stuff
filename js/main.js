2faceccaa97649e442c15a4b611f6c08719ec798
Dr Ric Colasanti <ricardo.colasanti@glasgow.ac.uk>
2023-10-09 14:43:18 +0100

    sorted with d3 csv laod

--- js/main.js
+++ js/main.js
@@ -1,48 +1,77 @@
 console.log("main");
+
+
+
+
+// fetch("../data/confusion.csv")
+//   .then((res) => res.text())
+//   .then((text) => {
+//     console.log();
+//     var paperdata = [["scopus","webOfScience","value"]]
+//     var allRows = text.split(/\r?\n|\r/);
+//     let topline = allRows[0].split(',')
+//     // console.log(topline);
+//     for (let i = 1; i < topline.length; i++) {
+//       topline[i] = topline[i].replace("%2c", " -").replace("\\", "").replace("\\&", "&")
+//       dictWebOf[topline[i]] = 0
+//     }
+//     for (let r = 1; r < allRows.length; r++) {
+//       let cols = allRows[r].split(',')
+//       let scops = cols[0]
+//       if (!(scops in dictScopus)) {
+//         dictScopus[scops] = 0
+//       }
+//       for (let c = 1; c < cols.length; c++) {
+//         let v = (parseInt(cols[c]) | 0)
+//         dictScopus[scops] += v
+//         dictWebOf[topline[c]] += v
+//         paperdata.push([cols[0], topline[c], v])
+//       }
+//     }
+//     for (var [key, _] of Object.entries(dictScopus)) {
+//       scopus.push(key)
+//     }
+//     for (var [key, _] of Object.entries(dictWebOf)) {
+//       webOfScience.push(key)
+//     }
+//     scopus.sort(function(a, b){return dictScopus[b] - dictScopus[a]}); 
+//     webOfScience.sort(function(a, b){return dictWebOf[b] - dictWebOf[a]}); 
+//     draw(paperdata)
+//   })
+//   .catch((e) => console.error(e));
+// // set the dimensions and margins of the graph
+// let c = 0
+
 var dictScopus = {}
 var dictWebOf = {}
-
 let scopus = []
 let webOfScience = []
+hdata=[]
+d3.csv("data/heat.csv", function(data){
+  if(!(scopus.includes(data.scopus))){
+    scopus.push(data.scopus)
+  }
+  if (!(data.scopus in dictScopus)) {
+    dictScopus[data.scopus] = 0
+  }
 
+  if(!(webOfScience.includes(data.webOfScience))){
+    webOfScience.push(data.webOfScience)
+  }
+  if (!(data.webOfScience in dictWebOf)) {
+    dictWebOf[data.webOfScience] = 0
+  }
+  dictWebOf[data.webOfScience]+=+data.value
+  dictScopus[data.scopus]+=+data.value
+  hdata.push([data.scopus,data.webOfScience,data.value])
 
-fetch("../data/confusion.csv")
-  .then((res) => res.text())
-  .then((text) => {
-    console.log(text);
-    var paperdata = [["scopus","webOfScience","value"]]
-    var allRows = text.split(/\r?\n|\r/);
-    let topline = allRows[0].split(',')
-    // console.log(topline);
-    for (let i = 1; i < topline.length; i++) {
-      topline[i] = topline[i].replace("%2c", " -").replace("\\", "").replace("\\&", "&")
-      dictWebOf[topline[i]] = 0
-    }
-    for (let r = 1; r < allRows.length; r++) {
-      let cols = allRows[r].split(',')
-      let scops = cols[0]
-      if (!(scops in dictScopus)) {
-        dictScopus[scops] = 0
-      }
-      for (let c = 1; c < cols.length; c++) {
-        let v = (parseInt(cols[c]) | 0)
-        dictScopus[scops] += v
-        dictWebOf[topline[c]] += v
-        paperdata.push([cols[0], topline[c], v])
-      }
-    }
-    for (var [key, _] of Object.entries(dictScopus)) {
-      scopus.push(key)
-    }
-    for (var [key, _] of Object.entries(dictWebOf)) {
-      webOfScience.push(key)
-    }
+}).then(function(){
     scopus.sort(function(a, b){return dictScopus[b] - dictScopus[a]}); 
     webOfScience.sort(function(a, b){return dictWebOf[b] - dictWebOf[a]}); 
-    draw(paperdata)
-  })
-  .catch((e) => console.error(e));
-// set the dimensions and margins of the graph
+    console.log(dictWebOf);
+    draw(hdata);
+});
+
 
 
 const margin = { top: 30, right: 30, bottom: 300, left: 300 },
@@ -58,7 +87,7 @@ const svg = d3.select("#my_dataviz")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
 // Labels of row and columns
-var draw = function (paperdata) {
+function draw(paperdata) {
     const x = d3.scaleBand()
         .range([0, width])
         .domain(scopus)
